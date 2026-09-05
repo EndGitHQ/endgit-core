34705e6995c11c91e640778846d55a5d2434ba9c
Rezn1r <rezn1r@proton.me>
2026-09-05 18:02:15 +0800

    feat: add Plugin and User schema definitions

---
+++ ent/schema/plugin.go
@@ -0,0 +1,113 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/lucsky/cuid"
+)
+
+// Plugin holds the schema definition for the Plugin entity.
+type Plugin struct {
+	ent.Schema
+}
+
+func (Plugin) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "plugins"},
+	}
+}
+
+// Fields of the Plugin.
+func (Plugin) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").
+			DefaultFunc(cuid.New).
+			Unique(),
+		field.String("name").
+			Unique(),
+		field.String("slug").
+			Unique(),
+		field.String("displayName"),
+		field.String("description"),
+		field.Text("longDescription").
+			Optional().
+			Nillable(),
+		field.String("iconUrl").
+			Optional().
+			Nillable(),
+		field.String("repoUrl").
+			Optional().
+			Nillable(),
+		field.String("license").
+			Optional().
+			Nillable(),
+		field.Strings("tags").
+			Default([]string{}),
+		field.Strings("keywords").
+			Default([]string{}),
+		field.Enum("pluginType").
+			Values("PYTHON", "CPP").
+			Default("PYTHON"),
+		field.Int("downloads").
+			Default(0),
+		field.Int("stars").
+			Default(0),
+		field.Int("commentCount").
+			Default(0),
+		field.Enum("status").
+			Values("DRAFT", "PENDING_REVIEW", "PUBLISHED", "REJECTED", "ARCHIVED").
+			Default("DRAFT"),
+		field.Text("statusReason").
+			Optional().
+			Nillable(),
+		field.Float("trustScore").
+			Default(0),
+		field.Enum("qualityBadge").
+			Values("NONE", "BRONZE", "SILVER", "GOLD").
+			Default("NONE"),
+		field.Bool("isVerified").
+			Default(false),
+		field.Bool("isFeatured").
+			Default(false),
+		field.String("reviewBuildId").
+			Optional().
+			Nillable(),
+		field.String("webhookId").
+			Optional().
+			Nillable(),
+		field.Time("createdAt").
+			Default(time.Now),
+		field.Time("updatedAt").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+
+		// Foreign key to User.
+		field.String("authorId"),
+	}
+}
+
+// Edges of the Plugin.
+func (Plugin) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("author", User.Type).
+			Field("authorId").
+			Unique().
+			Required(),
+
+		// TODO: we dont have the other schemas yet
+		// - versions Version[]
+		// - builds Build[]
+		// - autoChecks AutoCheck[]
+		// - reviews Review[]
+		// - reports Report[]
+		// - ratings Rating[]
+		// - comments PluginComment[]
+		// - moderationLogs ModerationLog[]
+		// - analytics PluginAnalytics[]
+	}
+}
---
+++ ent/schema/user.go
@@ -0,0 +1,80 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/lucsky/cuid"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+func (User) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "users"},
+	}
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").
+			DefaultFunc(cuid.New).
+			Unique(),
+		field.String("githubId").
+			Unique(),
+		field.String("username").
+			Unique(),
+		field.String("displayName").
+			Optional().
+			Nillable(),
+		field.String("email").
+			Optional().
+			Nillable(),
+		field.String("avatarUrl").
+			Optional().
+			Nillable(),
+		field.String("bio").
+			Optional().
+			Nillable(),
+		field.Enum("trustLevel").
+			Values("NEW", "TRUSTED", "MAINTAINER", "ADMIN").
+			Default("NEW"),
+		field.Int("trustScore").
+			Default(0),
+		field.Int("weeklyBuildQuota").
+			Default(50),
+		field.Int("weeklyBuildCount").
+			Default(0),
+		field.Time("quotaResetAt").
+			Default(time.Now),
+		field.Time("createdAt").
+			Default(time.Now),
+		field.Time("updatedAt").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("plugins", Plugin.Type).
+			Ref("author"),
+
+		// TODO: uncomment once other schemas are made
+		// - reviews Review[]
+		// - reports Report[]
+		// - ratings Rating[]
+		// - pluginComments PluginComment[]
+		// - sessions Session[]
+		// - accounts Account[]
+	}
+}
